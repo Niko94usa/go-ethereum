4ef9a3a308ff7414b290ddc86b8d3e5e305f66a3
Niko94usa <86586147+Niko94usa@users.noreply.github.com>
2023-09-02 17:46:08 +0300

    Update rlpdump_test.go

--- cmd/rlpdump/rlpdump_test.go
+++ cmd/rlpdump/rlpdump_test.go
@@ -1,4 +1,4 @@
-// Copyright 2021 The go-ethereum Authors
+asdfasdfs// Copyright 2021 The go-ethereum Authors
 // This file is part of go-ethereum.
 //
 // go-ethereum is free software: you can redistribute it and/or modify
